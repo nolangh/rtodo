7a9aacb7916b667f6d30f0e8a16a6100bf4debb5
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-05-11 21:31:43 -0600

    -a

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 //import "./App.css";
+import { useState } from "react";
 import "./css/app.module.css";
-import Input from "./input";
+//import Input from "./input";
+import Header from "./Header";
+import data from "./data.json";
+import Todo from "./Todo";
+import TodoList from "./TodoList";
 
 export default function App() {
-	//const [myTodos, set] = useState([]);
-	// setMyTodos((currTodos) => {
-	// 	currTodos.push("new todo");
-	// 	return currTodos;
-	// });
+	const [TodoList, setTodoList] = useState(data);
+
 	return (
 		<main className="main">
-			<h1> TODO List</h1>
-			<Input />
+			<Header />
 		</main>
 	);
 }
